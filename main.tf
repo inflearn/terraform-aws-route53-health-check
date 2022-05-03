d679e486419ca97cdf1eab39ed2e81887e2a50b8
sunb <sunb@inflab.com>
2022-05-03 18:51:57 +0900

    [DO-206] lookup 대신 try 사용하도록 수정

--- main.tf
+++ main.tf
@@ -2,16 +2,16 @@ resource "aws_route53_health_check" "this" {
   for_each = {
   for hc in var.health_checks :
   join("_", compact([
-    lookup(hc, "fqdn", ""), lookup(hc, "ip_address", ""), "${hc.port}_${hc.type}", lookup(hc, "resource_path", "")
+    try(hc.fqdn, ""), try(hc.ip_address, ""), "${hc.port}_${hc.type}", try(hc.resource_path, "")
   ])) => hc
   }
 
-  fqdn              = lookup(each.value, "fqdn", null)
-  ip_address        = lookup(each.value, "ip_address", null)
-  port              = lookup(each.value, "port", null)
+  fqdn              = try(each.value.fqdn, null)
+  ip_address        = try(each.value.ip_address, null)
+  port              = try(each.value.port, null)
   type              = each.value.type
-  resource_path     = lookup(each.value, "resource_path", null)
-  failure_threshold = lookup(each.value, "failure_threshold", null)
+  resource_path     = try(each.value.resource_path, null)
+  failure_threshold = try(each.value.failure_threshold, null)
   request_interval  = each.value.request_interval
   tags              = var.tags
 }
